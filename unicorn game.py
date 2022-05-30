f140215c3f4f1e356d8d9e00ec19d1f8d26f7325
ConnorWheelans <94933984+ConnorWheelans@users.noreply.github.com>
2022-05-30 12:56:04 +1200

    Update

--- unicorn game.py
+++ unicorn game.py
@@ -10,7 +10,7 @@ if (Play in Yes):
         
         
     elif (Instructions in No):
-        print ("the starting user will pay a intial amount to start their game the amoun the user should pay to play the game is $1 per round and the user should press enter to play the game the computer will gnerate a random token that is either a zebra, horse, honkey or a unicorn for the player  ")
+        print ("the starting player will pay a intial amount to start their game the amount the player should pay to play the game is $1 per round and the player should press enter to play the game the computer will generate a random token that is either a zebra, horse, donkey or a unicorn for the player the token should be displayed to the user if the token is a unicorn the user will win $5 and if the token is a zebra or a horse they will win 50c and if the token is a donkey the user will win nothing the maxium amount you can spend on the game at one time is $10 per session also the game allows you to continue/quit provided if you have lost all your money or not the game will supply the right feedback how much money you have won/lost after each round and how much money you have left")
         
     
     
